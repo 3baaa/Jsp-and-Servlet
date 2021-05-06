dd480487d1ed5a5a9cc0cb0fd6d5278067f571fb
b3aaa <rodrlehqk@naver.com>
2021-05-07 00:00:55 +0900

    설명부분추가를위한 삭제

--- chap22(이전chap22+게시판기능부분추가)/board/src/article/service/PermissionChecker.java
+++
@@ -1,9 +0,0 @@
-package article.service;
-
-import article.model.Article;
-
-public class PermissionChecker {
-	public static boolean canModify(String userid,Article article) {
-		return article.getWriter().getId().equals(userid);
-	}
-}
